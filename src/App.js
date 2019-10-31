800564ece4f84595db3f32494459ff317088eac6
Kran Thaweesith <kran.tha@truedigital.com>
2019-10-31 14:36:47 +0700

    create event

--- src/App.js
+++ src/App.js
@@ -4,12 +4,47 @@ import Content from './compoents/Content';
 import Footer from './compoents/Footer';
 
 class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            data: [],
+            message: "UnLike",
+            type: ""
+        };
+        this.changeMessage = this.changeMessage.bind(this);
+        this.insertData = this.insertData.bind(this);
+    }
+
+    changeMessage() {
+        this.setState({message: "Like"});
+    }
+
+    insertData() {
+        var item = 'React, ';
+        var myArray = this.state.data;
+        myArray.push(item);
+        this.setState({data: myArray});
+    }
+
+    changeType(event) {
+        this.setState({type: event.target.value})
+    }
+
     render() {
         return (
             <div>
                 <Header/>
                 <Content title="unit 1: " name="create App " date="- 30/10/19"/>
                 <Footer/>
+
+                <button onClick={this.changeMessage}>{this.state.message}</button>
+
+                <h2>{this.state.data}</h2>
+                <button onClick={this.insertData}>Insert</button>
+
+                <br/><br/>
+                <input type='text' onChange={this.changeType.bind(this)}/>
+                <h1>Welcome : {this.state.type}</h1>
             </div>
         );
     }
